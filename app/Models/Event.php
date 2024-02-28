0a48d5f17af12ef9eb4e331aed971afd6ec9ef41
Ahmed Esmail <107895995+AhmedEsmail91@users.noreply.github.com>
2024-02-28 22:39:37 +0200

    Event Management new

---
+++ app/Models/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Event extends Model
+{
+    use HasFactory;
+    public function user():BelongsTo{
+        // a user who will be the orgnizer.
+        return $this->belongsTo(User::class); // one to one relationship.
+    }
+    public function attendees(): HasMany{
+        return $this->hasMany(Attendee::class); // one event can be attended by many attendees. so one event has many attendees
+    }
+    
+}
+
---
+++ database/migrations/2024_02_27_160058_create_attendees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendees', function (Blueprint $table) {
+            $table->id();
+
+            // I have an Attendee table for for users who will attende all events.
+            $table->foreignIdFor(User::class);
+            // Having the id of the event for each user which will be attended.
+            $table->foreignIdFor(Event::class);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendees');
+    }
+};
